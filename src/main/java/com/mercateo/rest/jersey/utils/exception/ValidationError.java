b2ad6804f4d37ac87c9948399221bf652d00922c
Ibrahim Alzant <ibrahim.alzant@mercateo.com>
2018-01-24 17:32:48 +0100

    use the annotation from hibernate instead of javax 2

--- src/main/java/com/mercateo/rest/jersey/utils/exception/ValidationError.java
+++ src/main/java/com/mercateo/rest/jersey/utils/exception/ValidationError.java
@@ -7,11 +7,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ElementKind;
 import javax.validation.Path;
-import javax.validation.constraints.*;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.lang.annotation.Annotation;
 import java.util.Collection;
 
