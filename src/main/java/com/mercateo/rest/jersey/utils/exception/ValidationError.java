89bd59912f5f47595e830de0dcef31cf98ff1399
Ibrahim Alzant <ibrahim.alzant@mercateo.com>
2018-01-25 15:30:24 +0100

    retrieve error message from validation annotation message if not blank

--- src/main/java/com/mercateo/rest/jersey/utils/exception/ValidationError.java
+++ src/main/java/com/mercateo/rest/jersey/utils/exception/ValidationError.java
@@ -1,14 +1,7 @@
 package com.mercateo.rest.jersey.utils.exception;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.mercateo.rest.jersey.utils.validation.EnumValue;
-import com.mercateo.rest.jersey.utils.validation.NullOrNotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotBlank;
+import java.lang.annotation.Annotation;
+import java.util.Collection;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ElementKind;
@@ -18,8 +11,18 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.lang.annotation.Annotation;
-import java.util.Collection;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.mercateo.rest.jersey.utils.validation.EnumValue;
+import com.mercateo.rest.jersey.utils.validation.NullOrNotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
@@ -27,7 +30,7 @@ import java.util.Collection;
 public class ValidationError {
 
     @NonNull
-    ValidationErrorCode code;
+    String code;
 
     @NonNull
     String path;
@@ -35,39 +38,43 @@ public class ValidationError {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     Integer limit;
 
-    public static ValidationError of(ConstraintViolation<?> constraintViolation){
+    public static ValidationError of(ConstraintViolation<?> constraintViolation) {
         Annotation annotation = constraintViolation.getConstraintDescriptor().getAnnotation();
         String path = constructJsonPath(constraintViolation.getPropertyPath());
+        ValidationError error = null;
 
-        ValidationError error = new ValidationError(ValidationErrorCode.UNKNOWN, path);
-
-        if(annotation instanceof NotNull || annotation instanceof NotBlank || annotation instanceof AssertTrue){
-            return new ValidationError(ValidationErrorCode.REQUIRED, path);
-        } else if(annotation instanceof Size){
+        if (annotation instanceof NotNull || annotation instanceof NotBlank || annotation instanceof AssertTrue) {
+            return new ValidationError(ValidationErrorCode.REQUIRED.name(), path);
+        } else if (annotation instanceof Size) {
             final Size sizeAnnotation = (Size) annotation;
             final Object value = constraintViolation.getInvalidValue();
 
             if (value instanceof String && value.toString().length() < sizeAnnotation.min()) {
-                error = new ValidationError(ValidationErrorCode.MINLENGTH, path, sizeAnnotation.min());
+                error = new ValidationError(ValidationErrorCode.MINLENGTH.name(), path, sizeAnnotation.min());
             } else if (value instanceof String && value.toString().length() > sizeAnnotation.max()) {
-                error = new ValidationError(ValidationErrorCode.MAXLENGTH, path, sizeAnnotation.max());
+                error = new ValidationError(ValidationErrorCode.MAXLENGTH.name(), path, sizeAnnotation.max());
             } else if (value instanceof Collection && ((Collection<?>) value).size() < sizeAnnotation.min()) {
-                error = new ValidationError(ValidationErrorCode.MINITEMS, path, sizeAnnotation.min());
+                error = new ValidationError(ValidationErrorCode.MINITEMS.name(), path, sizeAnnotation.min());
             } else if (value instanceof Collection && ((Collection<?>) value).size() > sizeAnnotation.max()) {
-                error = new ValidationError(ValidationErrorCode.MAXITEMS, path, sizeAnnotation.max());
+                error = new ValidationError(ValidationErrorCode.MAXITEMS.name(), path, sizeAnnotation.max());
             }
-        } else if(annotation instanceof EnumValue){
-            error = new ValidationError(ValidationErrorCode.ENUM, path);
-        } else if(annotation instanceof NullOrNotBlank){
-            error = new ValidationError(ValidationErrorCode.INVALID, path);
+        } else if (annotation instanceof EnumValue) {
+            error = new ValidationError(ValidationErrorCode.ENUM.name(), path);
+        } else if (annotation instanceof NullOrNotBlank) {
+            error = new ValidationError(ValidationErrorCode.INVALID.name(), path);
         } else if (annotation instanceof Min) {
             final Min min = (Min) annotation;
-            error = new ValidationError(ValidationErrorCode.MINLENGTH, path, ((int) min.value()));
+            error = new ValidationError(ValidationErrorCode.MINLENGTH.name(), path, ((int) min.value()));
         } else if (annotation instanceof Max) {
             Max max = (Max) annotation;
-            error = new ValidationError(ValidationErrorCode.MAXLENGTH, path, ((int) max.value()));
+            error = new ValidationError(ValidationErrorCode.MAXLENGTH.name(), path, ((int) max.value()));
         } else if (annotation instanceof Email) {
-            error = new ValidationError(ValidationErrorCode.INVALID_EMAIL, path);
+            error = new ValidationError(ValidationErrorCode.INVALID_EMAIL.name(), path);
+        } else {
+            String code = constraintViolation.getMessage() != null && !constraintViolation.getMessage().isEmpty()
+                    ? constraintViolation.getMessage()
+                    : ValidationErrorCode.UNKNOWN.name();
+            error = new ValidationError(code, path);
         }
         return error;
     }
