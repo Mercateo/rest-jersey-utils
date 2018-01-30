4183a1c3fb4d1bad6f1ec41d24e600f35bfa6304
Ibrahim Alzant <ibrahim.alzant@mercateo.com>
2018-01-30 16:37:22 +0100

    change response fields order

--- src/main/java/com/mercateo/rest/jersey/utils/exception/ValidationExceptionJson.java
+++ src/main/java/com/mercateo/rest/jersey/utils/exception/ValidationExceptionJson.java
@@ -1,10 +1,12 @@
 package com.mercateo.rest.jersey.utils.exception;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@JsonPropertyOrder({ "type", "status","title","detail", "errors" })
 public class ValidationExceptionJson extends SimpleExceptionJson {
 
     final String type = "https://unite.eu/developers/errors/invalid";
